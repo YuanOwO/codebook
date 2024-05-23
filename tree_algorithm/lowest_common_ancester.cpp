eb3de7e632542bd70265176f99a428ee7fc8c75a
ShanC <shane931113@gmail.com>
2024-05-24 00:24:58 +0800

    LCA 模板修正

--- tree_algorithm/lowest_common_ancester.cpp
+++ tree_algorithm/lowest_common_ancester.cpp
@@ -1,27 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 1e5 + 5, logN = 20;
+const int N = 2e5 + 5, logN = 20;
 vector<int> g[N];
 
-int timing = 1;
+int timing = 1, logn, n;
 int in[N], out[N];
-int anc[N][logN];
+int anc[N][logN], depth[N];
 
 // 建立倍增表
-void build_table() {
-    for (int i = 1; i <= log2(N); i++) {
-        for (int now = 1; now <= N; now++) {
+void build_table(int n) {
+    logn = log2(n);
+    for (int i = 1; i < logN; i++) {
+        for (int now = 1; now <= n; now++) {
             anc[now][i] = anc[anc[now][i - 1]][i - 1];
         }
     }
 }
 
 // 建立時間戳記
-void dfs_init(int u) {
+// 跑一遍 DFS 得到誰是誰的父親
+void dfs_init(int u, int pre) {
     in[u] = timing++;       // 這時進入u
+    anc[u][0] = pre;        // now的0倍祖先是pre
     for (int nxt : g[u]) {  // 跑過所有孩子
-        dfs_init(nxt);
+        if (pre == nxt)
+            continue;
+        depth[nxt] = depth[u] + 1;
+        dfs_init(nxt, u);
     }
     out[u] = timing++;  // 這時離開u
 }
@@ -29,16 +35,6 @@ void dfs_init(int u) {
 // 判斷是否是祖先
 bool is_ancestor(int u, int v) {
     return (in[u] < in[v] && out[v] < out[u]);
-}
-
-// 跑一遍 DFS 得到誰是誰的父親
-void dfs(int now, int fa) {  // 現在的點、父節點
-    anc[now][0] = fa;        // now的0倍祖先是pre
-    for (auto i : g[now]) {
-        if (i == fa)
-            continue;
-        dfs(i, now);
-    }
 }
 
 // 找最低共同祖先
@@ -49,9 +45,11 @@ int getlca(int x, int y) {
         return y;
 
     // 判斷 2^logN, 2^(logN-1),...2^1, 2^0 倍祖先
-    for (int i = logN; i >= 0; i--) {
-        if (!is_ancestor(anc[x][i], y))  // 如果 2^i 倍祖先不是 v 的祖先
-            x = anc[x][i];               // 則往上移動
+    for (int i = logn; i >= 0; i--) {
+        // cout << anc[x][i] << '\n';
+        if (!is_ancestor(anc[x][i], y) &&
+            anc[x][i])      // 如果 2^i 倍祖先不是 v 的祖先
+            x = anc[x][i];  // 則往上移動
     }
     return anc[x][0];  // 回傳此點的父節點即為答案
 }
