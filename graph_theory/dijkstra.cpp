7c6c8a0dee689878a535f6e75de54185d935232f
ShanC <shane931113@gmail.com>
2024-07-24 20:08:16 +0800

    add bcc_tarjan.cpp

--- graph_theory/dijkstra.cpp
+++ graph_theory/dijkstra.cpp
@@ -3,17 +3,17 @@
 const int N = 2e5 + 5;
 vector<pair<int, int>> vec[N];
 bool vis[N];
-vector<int> dijkstra(int s) {    // 起點
+vector<int> dijkstra(int s) {  // 起點
     vector<int> dis(N);
     for (int i = 0; i < N; i++)  // 初始化
-        dis[i] = INF;            // 值要設為比可能的最短路徑權重還要大的值
+        dis[i] = INF;  // 值要設為比可能的最短路徑權重還要大的值
     dis[s] = 0;
     priority_queue<pii, vector<pii>, greater<pii>> pq;  // 以小到大排序
     pq.push({dis[s], s});
     while (pq.empty() == 0) {
         int u = pq.top().second;
         pq.pop();
-        if (vis[u]) //走過的不要再走
+        if (vis[u])  // 走過的不要再走
             continue;
         vis[u] = 1;
         for (auto i : vec[u]) {
