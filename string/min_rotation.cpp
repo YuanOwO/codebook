c35dcfe448dec542db3cb988685bf1a3f5e56567
ShanC <shane931113@gmail.com>
2024-07-25 16:39:36 +0800

    add min_rotation.cpp

---
+++ string/min_rotation.cpp
@@ -0,0 +1,21 @@
+// 用法: 欲處理之字串傳入min_rotation()，得一整數為最小rotation
+
+// rotate(begin(s),begin(s)+minRotation(s),end(s))
+// 上面註解可以直接讓一個字串做 rotate ，得到字典序最小的 rotation
+int min_rotation(string s) {
+    int a = 0, N = s.size();
+    s += s;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (a + j == i || s[a + j] < s[i + j]) {
+                i += max(0, j - 1);
+                break;
+            }
+            if (s[a + j] > s[i + j]) {
+                a = i;
+                break;
+            }
+        }
+    }
+    return a;
+}
