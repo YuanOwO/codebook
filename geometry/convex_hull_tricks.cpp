fab1cb411c63d3962cdf2859e49c57e0a47a9e5f
ShanC <shane931113@gmail.com>
2024-08-27 11:31:40 +0800

    add convex_hull_tricks

---
+++ geometry/convex_hull_tricks.cpp
@@ -0,0 +1,104 @@
+#define all(x) x.begin(), x.end()
+/**
+ * @brief 計算幾何一些常用功能
+ *
+ * 用法: 宣告 Convex 結構時傳入排序好的點陣列
+ * 功能:
+ * 1. inside() 判斷點是否在凸包裡 0: out, 1: on, 2: in
+ * 2. tangent() 傳入一根向量 回傳切點的 idx (close = true: 近)
+ * 3. tangent2() 傳入一點 回傳切線與凸包的交點
+ * 4. intersect() 傳入一條線 回傳交點 (2點 or 1點 or 沒點)
+ *
+ * 備註:
+ * 1. 注意資料型態
+ * 2. Convex 結構宣告時要傳入逆時針的陣列
+ *
+ */
+
+struct Convex {
+    int n;
+    vector<Pt> A, V, L, U;
+    Convex(const vector<Pt> &_A) : A(_A), n(_A.size()) {  // n >= 3
+        rotate(_A.begin(), min_element(all(_A)), _A.end());
+        // 最左的的點排最前面 有時可以不需要這一行
+        auto it = max_element(all(A));
+        L.assign(A.begin(), it + 1);
+        U.assign(it, A.end()), U.push_back(A[0]);
+        for (int i = 0; i < n; i++) {
+            V.push_back(A[(i + 1) % n] - A[i]);
+        }
+    }
+    int PtSide(Pt p, Line L) {
+        return dcmp((L.b - L.a) ^ (p - L.a));
+    }
+    int inside(Pt p, const vector<Pt> &h, auto f) {
+        auto it = lower_bound(all(h), p, f);
+        if (it == h.end())
+            return 0;
+        if (it == h.begin())
+            return p == *it;
+        return 1 - dcmp((p - *prev(it)) ^ (*it - *prev(it)));
+    }
+    // 1. whether a given point is inside the CH
+    // ret 0: out, 1: on, 2: in
+    int inside(Pt p) {
+        return min(inside(p, L, less{}), inside(p, U, greater{}));
+    }
+    static bool cmp(Pt a, Pt b) {
+        return dcmp(a ^ b) > 0;
+    }
+    // 2. Find tangent points of a given vector
+    // ret the idx of far/closer tangent point
+    int tangent(Pt v, bool close = true) {
+        assert(v != Pt{});
+        auto l = V.begin(), r = V.begin() + L.size() - 1;
+        if (v < Pt{})
+            l = r, r = V.end();
+        if (close)
+            return (lower_bound(l, r, v, cmp) - V.begin()) % n;
+        return (upper_bound(l, r, v, cmp) - V.begin()) % n;
+    }
+    // 3. Find 2 tang pts on CH of a given outside point
+    // return index of tangent points
+    // return {-1, -1} if inside CH
+    array<int, 2> tangent2(Pt p) {
+        array<int, 2> t{-1, -1};
+        if (inside(p) == 2)
+            return t;
+        if (auto it = lower_bound(all(L), p);
+            it != L.end() and p == *it) {
+            int s = it - L.begin();
+            return {(s + 1) % n, (s - 1 + n) % n};
+        }
+        if (auto it = lower_bound(all(U), p, greater{});
+            it != U.end() and p == *it) {
+            int s = it - U.begin() + L.size() - 1;
+            return {(s + 1) % n, (s - 1 + n) % n};
+        }
+        for (int i = 0; i != t[0]; i = tangent((A[t[0] = i] - p), 0))
+            ;
+        for (int i = 0; i != t[1]; i = tangent((p - A[t[1] = i]), 1))
+            ;
+        return t;
+    }
+    int find(int l, int r, Line L) {
+        if (r < l)
+            r += n;
+        int s = PtSide(A[l % n], L);
+        return *ranges::partition_point(views::iota(l, r), [&](int m) {
+            return PtSide(A[m % n], L) == s;
+        }) - 1;
+    }
+    // 4. Find intersection point of a given line
+    // intersection is on edge (i, next(i))
+    vector<int> intersect(Line L) {
+        int l = tangent(L.a - L.b), r = tangent(L.b - L.a);
+        if (PtSide(A[l], L) == 0)
+            return {l};
+        if (PtSide(A[r], L) == 0)
+            return {r};
+        if (PtSide(A[l], L) * PtSide(A[r], L) > 0)
+            return {};
+        return {find(l, r, L) % n, find(r, l, L) % n};
+    }
+};
