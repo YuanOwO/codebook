08a450dadf25cddfcb44c9590b0dfb1226a55ea7
ShanC <shane931113@gmail.com>
2024-08-27 08:02:56 +0800

    add discription

--- geometry/picks_theory.cpp
+++ geometry/picks_theory.cpp
@@ -1,22 +1,20 @@
-#include <bits/stdc++.h>
 #define ll long long
-using namespace std;
-
 const int MAXN = 1e5 + 5;
-struct Point {
-    ll x, y;
 
-    Point operator-(const Point &a) {
-        return {x - a.x, y - a.y};
-    }
+/**
+ * @brief 皮克定理
+ * A = i + b/2 − 1
+ * 2A = 2i + b - 2 --- 同乘 2
+ * i = A - b/2 + 1
+ * A: 面積; b: 圖形內部的點數量; i: 圖形線上的點數量
+ *
+ * 用法: 先推公式 然後需要哪個就丟哪個函式
+ *
+ */
 
-    friend ll cross(Point a, Point b, Point o) {
-        Point p1 = a - o, p2 = b - o;
-        return p1.x * p2.y - p1.y * p2.x;
-    }
-} p[MAXN];
+vector<Pt> p;
 
-ll compute_area(int n) {  // A*2
+ll compute_area(int n) {  // A * 2
     ll area = 0;
     for (int i = 1; i < n; i++)
         area += cross(p[i], p[(i + 1) % n], p[0]);
@@ -29,7 +27,7 @@ ll compute_pt_on_line(int n) {  // b
     for (int i = 0; i < n; i++)
         pt += __gcd(abs(p[i].x - p[(i + 1) % n].x),
                     abs(p[i].y - p[(i + 1) % n].y));
-    return pt;
+    return pt;  // 這是網路上找的
 }
 
 ll compute_inner_pt(int n) {  // i
