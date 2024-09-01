ea6100e3f0dd294dcefba80151af594d22b99b80
ShanC <shane931113@gmail.com>
2024-09-01 20:59:48 +0800

    fixed bugs

--- graph_theory/floyd_warshall.cpp
+++ graph_theory/floyd_warshall.cpp
@@ -6,7 +6,10 @@ const int N = 2e3 + 5;
  * 全圖最短路徑
  * 圖上找任兩點都有最短路徑長
  *
- * 用法: 呼叫 floyd_warshall(n) 其中 n 為節點數
+ * 用法:
+ * 1. init(n) 初始化 其中 n 為節點數
+ * 2. 輸入 dis[u][v]: 從 u 到 v 的原始距離
+ * 3. 呼叫 floyd_warshall(n) 算答案
  * 結果: dis[u][v]: 從 u 到 v 的最短路徑長
  * 備註:
  * - 注意資料型態
@@ -17,13 +20,14 @@ const int N = 2e3 + 5;
  */
 
 int dis[N][N];
-void floyd_warshall(int n) {
+void init(int n) {
     for (int i = 0; i <= n; i++) {  // 初始化
         for (int j = 0; j <= n; j++)
             dis[i][j] = INF;
         dis[i][i] = 0;
     }
-
+}
+void floyd_warshall(int n) {
     for (int k = 0; k < n; k++) {  // 窮舉中繼點 k
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {  // 窮舉點對 (i, j)
