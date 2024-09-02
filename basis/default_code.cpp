b6fe3f9e60a73dc6bb05eb9a1ed25818fdf4f0e4
ShanC <shane931113@gmail.com>
2024-09-02 17:47:54 +0800

    add basis

---
+++ basis/default_code.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int, int>
+#define INF 0x7f7f7f7f7f7f7f7fLL
+using namespace std;
+const int MXN = 2e6 + 5;
+const int MOD = 1e9 + 7;
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 0;
+}
