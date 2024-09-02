fbbb8ce96c7df9243e8c51e61fac0e62d7688d90
ShanC <shane931113@gmail.com>
2024-09-02 19:31:57 +0800

    fixed tree shit

--- tree_algorithm/deep_first_search.cpp
+++ tree_algorithm/deep_first_search.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-const int MAXN = 1e6 + 5;
 vector<int> tree[MAXN];
-
 void dfs(int u, int pre) {
     for (int v : tree[u]) {
         if (pre == v)
--- tree_algorithm/dsu_on_tree.cpp
+++ tree_algorithm/dsu_on_tree.cpp
@@ -1,6 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 int find(int x) {
     if (f[x] == x)
         return x;
