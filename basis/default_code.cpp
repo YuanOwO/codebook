26ac3ce465d59b7d36986592a2772e1845dd7c06
YuanOwO <yuan@yuanowo.xyz>
2024-11-11 22:44:23 +0800

    Codebook
    Codebook 排版

--- basis/default_code.cpp
+++ basis/default_code.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
-#define REP(i, n) for (int i = 0; i < n; i++)
-#define PB push_back
-#define SZ(x) (int)x.size()
 #define ll long long
 #define ld long double
 #define pii pair<int, int>
-#define INF 0x7f7f7f7f7f7f7f7fLL
-#define all(x) x.begin(), x.end()
 #define D double
+#define INF 0x7f7f7f7f7f7f7f7fLL
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define PB push_back
+#define SZ(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
 using namespace std;
-const int MXN = 2e6 + 5;
+const int MAX_N = 2e6 + 5;
 const int MOD = 1e9 + 7;
-int main() {
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    return 0;
+signed main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  return 0;
 }
