c378fba0c60b13455b586c592453bf229d85f314
ShanC <shane931113@gmail.com>
2024-11-15 07:25:13 +0800

    add farthest_distance in codebook

--- geometry/farthest_distance(convex_hull).cpp
+++ geometry/farthest_distance(convex_hull).cpp
@@ -1,46 +1,5 @@
-struct Pt {
-  int x, y;
-  Pt() {}
-  Pt(int _x, int _y) { x = _x, y = _y; }
-  Pt operator-(const Pt& a) {
-    return Pt(x - a.x, y - a.y);
-  }
-  bool operator<(const Pt& a) const {
-    return x < a.x || (x == a.x && y < a.y);
-  }
-  friend int cross(Pt& o, Pt& a, Pt& b) {
-    Pt lhs = o - a, rhs = o - b;
-    return lhs.x * rhs.y - lhs.y * rhs.x;
-  }
-  friend double distance(Pt a, Pt b) {
-    return (a.x - b.x) * (a.x - b.x) +
-           (a.y - b.y) * (a.y - b.y);
-  }
-};
-vector<Pt> convex_hull(vector<Pt> hull) {
-  sort(hull.begin(), hull.end());
-  int top = 0;
-  vector<Pt> stk;
-  for (int i = 0; i < hull.size(); i++) {
-    while (top >= 2 && cross(stk[top - 2], stk[top - 1],
-                             hull[i]) <= 0)
-      stk.pop_back(), top--;
-    stk.push_back(hull[i]);
-    top++;
-  }
-  for (int i = hull.size() - 2, t = top + 1; i >= 0;
-       i--) {
-    while (top >= t && cross(stk[top - 2], stk[top - 1],
-                             hull[i]) <= 0)
-      stk.pop_back(), top--;
-    stk.push_back(hull[i]);
-    top++;
-  }
-  stk.pop_back();
-  return stk;
-}
 double FarthestPair(vector<Pt> arr) {
-  double ret = 0;
+  double ret = 0;  // 傳入凸包
   int sz = sz;
   for (int i = 0, j = i + 1; i < sz; i++) {
     while (distance(arr[i], arr[j]) <=
