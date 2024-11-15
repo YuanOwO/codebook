1f1a2106dc44a857eaf90ffbc98bacb71bca5931
YuanOwO <yuan@yuanowo.xyz>
2024-11-15 10:20:43 +0800

    修改區間DP說明

--- DP/digit.cpp
+++ DP/digit.cpp
@@ -1,15 +1,3 @@
-// dp[位數][狀態]
-// dp[pos][state]:
-// 定義為目前位數在前導狀態為state的時候的計數 ex:
-// 求數字沒有出現66的數量 l~r
-// -> dp[pos][1] 可表示計算pos個位數在前導出現一個6的計數
-// -> dp[3][1] 則計算  6XXX
-// 模板的pos是反過來的，但不影響(只是用來dp記憶用)
-// pos: 目前位數
-// state: 前導狀態
-// lead: 是否有前導0
-// (大部分題目不用但有些數字EX:00146如果有影響時要考慮)
-// limit: 使否窮舉有被num限制
 vector<int> num;
 int dp[20][state];
 int dfs(int pos, int state, bool lead, bool limit) {
