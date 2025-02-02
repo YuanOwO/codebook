db5652a2593f0e711dfa74b1fb48a211e4b6d4d1
ShanC <shane931113@gmail.com>
2025-02-02 08:09:45 +0800

    use list (vector) instead of matrix

--- graph_theory/kuhn_algorithm.cpp
+++ graph_theory/kuhn_algorithm.cpp
@@ -1,15 +1,15 @@
-bool vis[MXN], adj[MXN][MXN];
-int S[MXN];  // 其中 adj 為鄰接表, S 為紀錄這個點與誰匹配
-int n, p, ans;  // n: 左集合數量, p: 右集合數量
-bool tag[MXN];  // 紀錄是否被配對過
+vector<int> g[MXN];
+bool vis[MXN];
+int S[MXN];  // S 為紀錄這個點與誰匹配
+int n, ans;  // n: 左集合數量, ans: 紀錄答案
 
-bool dfs(int u) {                         // 找最大匹配
-  for (int i = n + 1; i <= n + p; i++) {  // 跑右集合
-    if (adj[u][i] && !vis[i]) {       // 有連通且未拜訪
-      vis[i] = 1;                     // 紀錄是否走過
-      if (S[i] == -1 || dfs(S[i])) {  // 紀錄匹配
-        S[i] = u, tag[u] = true;
-        return true;  // 反轉匹配邊以及未匹配邊的狀態
+bool dfs(int u) {  // 找最大匹配
+  for (int &v : g[u]) {
+    if (!vis[v]) {
+      vis[v] = true;
+      if (S[v] == -1 || dfs(S[v])) {
+        S[v] = u;
+        return true;
       }
     }
   }
@@ -17,7 +17,7 @@ bool dfs(int u) {                         // 找最大匹配
 }
 
 void solve() {  // 記得每次使用需清空 vis
-  for (int i = 1; i <= n + p; i++) S[i] = -1;
+  memset(S, -1, sizeof(S));
   ans = 0;
   for (int i = 1; i <= n; i++) {  // 跑左集合
     memset(vis, 0, sizeof(vis));
