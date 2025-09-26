3417981e3549808787672f6d304ab6f87fd2df74
Simon_QwQ <70833874+simon0x0@users.noreply.github.com>
2025-09-26 21:03:01 +0800

    add mobius function

---
+++ math/MobiusFunction.cpp
@@ -0,0 +1,22 @@
+vector<int> pri;
+bool not_prime[N];
+int mu[N];
+
+void pre(int n) {
+  mu[1] = 1;
+  for (int i = 2; i <= n; ++i) {
+    if (!not_prime[i]) {
+      mu[i] = -1;
+      pri.push_back(i);
+    }
+    for (int pri_j : pri) {
+      if (i * pri_j > n) break;
+      not_prime[i * pri_j] = true;
+      if (i % pri_j == 0) {
+        mu[i * pri_j] = 0;
+        break;
+      }
+      mu[i * pri_j] = -mu[i];
+    }
+  }
+}
