46bb1751ae860ba6bc11b885b9211ea2c0250166
ShanC <shane931113@gmail.com>
2025-11-13 21:17:53 +0800

    fixed something

--- basis/default_code.cpp
+++ basis/default_code.cpp
@@ -1,21 +1,3 @@
 // g++args: -std=c++17 -Wall -Wshadow -fsanitize=undefined
 #pragma GCC optimize("O3,unroll-loops")
 #pragma target optimize("avx2,bmi,bmi2,lzcnt,popcnt")
-#include <bits/stdc++.h>
-#define ll long long
-#define lll __int128
-#define ld long double
-#define pii pair<int, int>
-#define D double
-#define INF 0x7f7f7f7f7f7f7f7fLL
-#define REP(i, n) for (int i = 0; i < n; i++)
-#define PB push_back
-#define SZ(x) (int)(x).size()
-#define all(x) (x).begin(), (x).end()
-using namespace std;
-const int MXN = 2e6 + 5;
-const int MOD = 1e9 + 7;
-signed main() {
-  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-  return 0;
-}
