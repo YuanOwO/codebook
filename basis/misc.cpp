f3ebb3810d0a7784b64826e856d7aadcd59ba916
YuanOwO <yuan@yuanowo.xyz>
2025-11-13 23:22:33 +0800

    ICPC 2025

--- basis/misc.cpp
+++ basis/misc.cpp
@@ -9,3 +9,4 @@ __builtin_popcountll;  // 二進位有幾個1
 __builtin_clzll;       // 左起第一個1之前0的個數
 __builtin_parityll;    // popcount的奇偶性
 __builtin_mul_overflow(a, b, &h)  // a*b是否溢位
+__lg(x);               // floor(log2(x))
--- basis/rand.cpp
+++ basis/rand.cpp
@@ -1,6 +1,6 @@
 // chrono::steady_clock::now().time_since_epoch().count();
 mt19937 gen(/*seed*/ hash<string>()("ShanC_orz"));
-mt19937_64 mt(chrono::steady_clock::now().time_since_epoch().count());
+mt19937_64 mt(/*seed*/ 114514);
 int randint(int a, int b) {  // [a, b]
   return uniform_int_distribution<int>(a, b)(gen);
 }
